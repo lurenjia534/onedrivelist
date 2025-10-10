30fe5a179b1807f6ec9f71c17e86497d8c20ea02
luren <lurenjia534@outlook.com>
2025-10-11 06:45:41 +0800

    feat: Export upload-related functionality to support file uploads.

--- src/lib/onedrive.ts
+++ src/lib/onedrive.ts
@@ -7,6 +7,10 @@ export {
   deleteDriveItem,
   createFolder,
   renameDriveItem,
+  uploadSmallFileToFolder,
+  createUploadSessionToFolder,
+  uploadLargeFileToFolder,
+  SMALL_FILE_MAX_BYTES,
 } from "@/services/onedrive/repo";
 
 export { getAccessToken } from "@/services/onedrive/client";
